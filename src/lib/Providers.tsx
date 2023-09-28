0801f7dbb7190d1576c1d37c65211d51c219f88d
Naimur <naeemhasan28@gmail.com>
2023-09-28 21:18:47 +0600

    setup: ant design

--- src/lib/Providers.tsx
+++ src/lib/Providers.tsx
@@ -3,11 +3,16 @@
 import { store } from '@/redux/store'
 import { ReactNode } from 'react'
 import { Provider } from 'react-redux'
+import StyledComponentsRegistry from './AntdRegistry'
 
 interface Props {
   children: ReactNode
 }
 
 export default function Providers({ children }: Props) {
-  return <Provider store={store}>{children}</Provider>
+  return (
+    <Provider store={store}>
+      <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+    </Provider>
+  )
 }
